890af22de6ddefe5e553d3fb56cecdba7d38f75f
Noah Lin <noah@c-link.com.tw>
2024-09-06 14:21:53 +0800

    update: setup.sh

--- setup.sh
+++ setup.sh
@@ -1,57 +1,56 @@
 #!/bin/bash
 
 # dependecies
-apt-get update
-apt-get install -y curl ca-certificates gcc python3.11
+#apt-get update
+#apt-get install -y curl ca-certificates gcc
 
 # gpu drivers
-ubuntu-drivers install
+#ubuntu-drivers install
 
 # NVIDIA cuda toolkit
-lspci | grep -i nvidia
-apt-get install linux-headers-"$(uname -r)"
-apt-key del 7fa2af80
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
-dpkg -i cuda-keyring_1.1-1_all.deb
-apt-get update
-apt-get -y install cuda-toolkit-12-6
-apt-get install -y nvidia-open
+#lspci | grep -i nvidia
+#apt-get install linux-headers-"$(uname -r)"
+#apt-key del 7fa2af80
+#wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+#dpkg -i cuda-keyring_1.1-1_all.deb
+#apt-get update
+#apt-get -y install cuda-toolkit-12-6
+#apt-get install -y nvidia-open
 
 # conda
-mkdir -p ~/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-rm -rf ~/miniconda3/miniconda.sh
-~/miniconda3/bin/conda init bash
+mkdir -p /home/ai/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /home/ai/miniconda3/miniconda.sh
+bash /home/ai/miniconda3/miniconda.sh -b -u -p /home/ai/miniconda3
+rm -rf /home/ai/miniconda3/miniconda.sh
+/home/ai/miniconda3/bin/conda init bash
+bash
 
 # nodejs
-curl -fsSL https://deb.nodesource.com/setup_22.x -o nodesource_setup.sh
-bash nodesource_setup.sh
-apt-get install -y nodejs
+#curl -fsSL https://deb.nodesource.com/setup_22.x -o nodesource_setup.sh
+#bash nodesource_setup.sh
+#apt-get install -y nodejs
 
 # docker
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
-echo \
-	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
-	tee /etc/apt/sources.list.d/docker.list >/dev/null
+#install -m 0755 -d /etc/apt/keyrings
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+#chmod a+r /etc/apt/keyrings/docker.asc
+#echo \
+#	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+#  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+#	tee /etc/apt/sources.list.d/docker.list >/dev/null
 
 # ollama
-curl -fsSL https://ollama.com/install.sh | sh
-ollama --version
+#curl -fsSL https://ollama.com/install.sh | sh
+#ollama --version
 
 # clink-llm
-git clone https://github.com/popshia/clink-llm
-cd clink-llm || exit
-npm install && npm run build
-cd ./backend || exit
+#npm install && npm run build
+#cd /home/ai/repos/clink-llm/backend || exit
 
 # create conda env
-conda create --name clink-llm python=3.11
-conda activate clink-llm
-pip install -r requirements.txt -U
+#conda create -n clink-llm python=3.11 -y
+#conda activate clink-llm
+#pip install -r requirements.txt -U
 
 # start
-bash start.sh
+#bash /home/ai/repos/clink-llm/backend/start.sh
